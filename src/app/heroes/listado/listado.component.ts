cb0dcc5efcabc42d4e7136ba2cab5a29fb1a965e
carreratomas <tca.carrera@gmail.com>
2022-07-29 13:35:35 -0300

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,20 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+
+})
+export class ListadoComponent{
+
+  heroes: string[] = ['SpiderMan', 'IronMan', 'Hulk', 'Thor', 'Captain America'];
+  heroeBorrado: string = '';
+
+  borrarHeroe(){
+
+    this.heroeBorrado = this.heroes.shift() || '';
+    
+
+  }
+
+}
